7a95702145c1c42a9f56ef5a4ac565fc2bd6a9e5
tanvimori1811 <tanvimori70454091@gmail.com>
2026-03-04 21:42:52 +0530

    Added UC7 Deque Based Palindrome

---
+++ App/src/UseCase7PalindromeCheckerApp.java
@@ -0,0 +1,30 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class UseCase7PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "refer";
+
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input.toCharArray()) {
+            deque.add(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (deque.size() > 1) {
+
+            if (deque.removeFirst() != deque.removeLast()) {
+                isPalindrome = false;
+                break;
+            }
+
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
