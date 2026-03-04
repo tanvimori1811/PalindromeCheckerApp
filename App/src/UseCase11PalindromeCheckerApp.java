1a618655ce4923ff19f58418a09e0c62833abf10
tanvimori1811 <tanvimori70454091@gmail.com>
2026-03-04 22:04:33 +0530

    Added UC11 OOP Palindrome Service

---
+++ App/src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,35 @@
+public class UseCase11PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "radar";
+
+        PalindromeService service = new PalindromeService();
+
+        boolean result = service.checkPalindrome(input);
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+    }
+}
+
+class PalindromeService {
+
+    public boolean checkPalindrome(String input) {
+
+        int start = 0;
+        int end = input.length() - 1;
+
+        while (start < end) {
+
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
